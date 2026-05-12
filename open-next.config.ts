1fa68112f184954d67cf5f63a4d1359db1c41b1f
Devesh <deveshshetty66@gmail.com>
2026-05-12 12:24:55 +0530

    Add Cloudflare Pages configuration

---
+++ open-next.config.ts
@@ -0,0 +1,18 @@
+import type { OpenNextConfig } from "@opennextjs/cloudflare";
+
+const config: OpenNextConfig = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  middleware: {
+    external: true,
+  },
+};
+
+export default config;
